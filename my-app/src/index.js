9d3471ba0f64034ac02b04ec4ad20cab6c46986d
aidan-le-beard <hihihi349@yahoo.com>
2024-05-10 16:24:58 -0400

    initial blank tic-tac-toe board

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+// initial comment
+
 class Square extends React.Component {
     render() {
       return (
